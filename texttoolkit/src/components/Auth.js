cd3acf1d59d0a3b36eb33a570a9eebe0c04f8f7d
Rohit Chaurasia <rohiit.chaurasiya@gmail.com>
2023-08-14 15:38:56 +0530

    Update Auth.js

--- texttoolkit/src/components/Auth.js
+++ texttoolkit/src/components/Auth.js
@@ -22,7 +22,7 @@ const Auth=()=> {
         
         e.preventDefault();
         
-        const response=await fetch('text-toolkit-backend.vercel.app/signin',{
+        const response=await fetch('https://text-toolkit-backend.vercel.app/signin',{
         method:'POST',
         body:JSON.stringify(form),
         headers:{
