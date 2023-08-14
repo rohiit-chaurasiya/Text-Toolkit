486ac786736795c622d0305e416aa3c6ce8bd746
Rohit Chaurasia <rohiit.chaurasiya@gmail.com>
2023-08-14 15:38:38 +0530

    Update SignUp.js

--- texttoolkit/src/components/SignUp.js
+++ texttoolkit/src/components/SignUp.js
@@ -38,7 +38,7 @@ const SignUp=()=> {
         e.preventDefault();
         validatePassword();
         
-        const response=await fetch('text-toolkit-backend.vercel.app/signup',{
+        const response=await fetch('https://text-toolkit-backend.vercel.app/signup',{
         method:'POST',
         body:JSON.stringify(form),
         headers:{
