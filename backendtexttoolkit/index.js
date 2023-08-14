ab6f680f7f4b3a283add021fb56ed6aafcc54241
Rohit Chaurasia <rohiit.chaurasiya@gmail.com>
2023-08-14 16:59:51 +0530

    Update index.js

--- backendtexttoolkit/index.js
+++ backendtexttoolkit/index.js
@@ -7,7 +7,7 @@ const server=express();
 
 server.use(cors(
   {
-    origin:["https://text-toolkit.vercel.app"],
+    origin:["https://text-toolkit.vercel.app/"],
     methos:["POST", "GET"],
     credentials: true
   }
