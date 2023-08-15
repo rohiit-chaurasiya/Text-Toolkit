7ef9056089608a51625edd993224a2e2221c0f98
Rohit Chaurasia <rohiit.chaurasiya@gmail.com>
2023-08-15 20:37:48 +0530

    Update index.js

--- servertexttoolkit/index.js
+++ servertexttoolkit/index.js
@@ -39,6 +39,15 @@ app.get("/", (req, res) => {
 
 app.post('/signin',async (req,res)=>{
   try {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
 
     const { userName, userPassword } = req.body;
     const user = await User.findOne({ userName });
@@ -80,6 +89,15 @@ app.post('/signin',async (req,res)=>{
 
 
 app.post('/signup',async (req,res)=>{
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
   let user=new User();
   user.userName=req.body.userName;
   user.userPassword=req.body.userPassword;
